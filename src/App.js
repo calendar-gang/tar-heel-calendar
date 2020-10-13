367b4405230763215e0debd2f4771fba67709910
Ezri White <ezri@live.unc.edu>
2020-10-13 17:27:41 -0400

    Set up basic calendar view

--- src/App.js
+++ src/App.js
@@ -6,10 +6,289 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 class="title is-1" >TAR HEEL CALENDAR</h1>
+      </header>
+      <div class="container">
+        <div class="tabs is-centered">
+          <ul>
+            <li ><a>Day</a></li>
+            <li class="is-active"><a>Week</a></li>
+            <li><a>Month</a></li>
+            <li><a>Year</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="calendar">
+        <div class="container">
+          <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+            <thead>
+              <tr class="is-bordered">
+                <th></th>
+                <th>Sunday</th>
+                <th>Monday</th>
+                <th>Tuesday</th>
+                <th>Wednesday</th>
+                <th>Thursday</th>
+                <th>Friday</th>
+                <th>Saturday</th>
+              </tr>
+            </thead>
+            <tr>
+              <th>12am</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>1am</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>2am</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>3am</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>4am</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>5am</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>6am</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>7am</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>8am</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>9am</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>10am</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>11am</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>12pm</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>1pm</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>2pm</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>3pm</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>4pm</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>5pm</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>6pm</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>7pm</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>8pm</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>9pm</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>10pm</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+            <tr>
+              <th>11pm</th>
+              <td>Prepare for exam</td>
+              <td>Fail 521 exam</td>
+              <td>Move on with life</td>
+              <td>Anticipate future</td>
+              <td>Who cares about future, it's Friday!</td>
+              <td>Sleep</td>
+              <td>Hello</td>
+            </tr>
+
+          </table>
+        </div>
+      </div>
+
+      <footer class="footer">
+        <div class="content has-text-centered">
+          <p>
+            <strong> Tar Heel Calendar</strong> by Victoria Helenae Hoffmanne IV, Ezri, Eric Schneider II, Alfred.
+    </p>
+        </div>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,28 +298,13 @@ function App() {
           Learn React
         </a>
         <p>This is a calendar. Very fancy.</p>
-      </header>
-      <table>
-        <tr>
-          <th>Sunday</th>
-          <th>Monday</th>
-          <th>Tuesday</th>
-          <th>Wednesday</th>
-          <th>Thursday</th>
-          <th>Friday</th>
-          <th>Saturday</th>
-        </tr>
-        <tr>
-          <td>Sleep all day</td>
-          <td>Prepare for exam</td>
-          <td>Fail 521 exam</td>
-          <td>Move on with life</td>
-          <td>Anticipate future</td>
-          <td>Who cares about future, it's Friday!</td>
-          <td>Sleep</td>
-        </tr>
-      </table>
-    </div>
+
+
+
+
+      </footer>
+    </div >
+
   );
 }
 
