9cd2f9290e6c4e65ba83a9fcfbcffc00f8f9f0c3
Alfred Mathew <alfredmathew@outlook.com>
2020-11-14 19:27:42 -0500

    basic impl of month view

--- src/components/month.jsx
+++ src/components/month.jsx
@@ -1,11 +1,127 @@
 import React, {Component} from 'react';
 
 class Month extends Component {
-    state = {}
+    state = {};
+
+    toggleEditBox(event) {
+        event.persist();
+        let td = event.nativeEvent.path[0];
+        let edit_box = document.getElementById("edit-box");
+        let content = document.getElementById("edit-box-content");
+        if(edit_box.className == "modal is-active") {
+            edit_box.className = "modal";
+            while(content.firstChild) {
+                content.removeChild(content.firstChild);
+            }
+        } else {
+            edit_box.className = "modal is-active";
+            let task = document.createElement("p");
+            task.innerHTML = td.innerHTML;
+            content.append(task);
+        }
+    }
+
+    createEditBox() {
+        return (
+            <div id="edit-box" class="modal">
+                <div class="modal-background"></div>
+                    <div class="modal-card">
+                        <header class="modal-card-head">
+                        <p class="modal-card-title">Here Are Your Tasks!</p>
+                        <button onClick={this.toggleEditBox} class="delete" aria-label="close"></button>
+                        </header>
+                        <section id="edit-box-content" class="modal-card-body">
+                        </section>
+                        <footer class="modal-card-foot">
+                        <button class="button is-warning">Edit</button>
+                        <button class="button is-success">Save changes</button>
+                        <button onClick={this.toggleEditBox} class="button">Cancel</button>
+                        </footer>
+                    </div>
+                </div>
+                
+        )
+    }
+
+    _renderRowByDay(week_position) {
+        let date = new Date();
+        let year = date.getFullYear();
+        let month = date.getMonth();
+        let day_count = this.numDays(year, month) - 1;
+        let day_num = (month, day, year) => (new Date(year, month, day)).getDay();
+        let rows = [];
+        for(let i = 0; i < 7; i++){
+            let day = (week_position*7 + i + 1) > day_count ? "" : (week_position*7 + i + 1);
+            if(day != "") {
+                let real_day_num = day_num(month, day, year);
+                if(real_day_num != i) {
+                    day = "";
+                }
+            }
+            rows.push(
+                <td onClick={this.toggleEditBox}>{day}</td>
+            );
+        }
+        return (
+            <tr>
+                {rows}
+            </tr>
+        )
+    }
+
+    numDays(year, month) {
+        return (new Date(year, month, 0)).getDate();
+    }
+
+    _renderBody() {
+        let rows = [];
+        let date = new Date();
+        let year = date.getFullYear();
+        let month = date.getMonth();
+        for(let i = 0; i < (this.numDays(year, month) % 7) + 2; i++) {
+            rows.push(this._renderRowByDay(i));
+        }
+        return (
+            <tbody>
+                {rows}
+            </tbody>
+        )
+    }
 
     render() {
-        return(
-            <p>fill me in!</p>
+        const monthNames = ["January", "February", "March", "April", "May", "June",
+                            "July", "August", "September", "October", "November", "December"];
+        
+        return (
+            <div className="calendar">
+                <section class="hero is-primary">
+                    <div class="hero-body">
+                        <div class="container">
+                            <h1 class="title">
+                                {monthNames[(new Date()).getMonth()]}
+                            </h1>
+                            <h1 class="subtitle">A Monthly View Designed Just For You!</h1>
+                        </div>
+                    </div>
+                </section>
+                <div className="container">
+                <table className="table is-bordered is-narrow is-hoverable is-fullwidth">
+                    <thead>
+                    <tr className="is-bordered">
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                    </tr>
+                    </thead>
+                    {this._renderBody()}
+                </table>
+                {this.createEditBox()}
+            </div>
+            </div>
         )
     }
 }
