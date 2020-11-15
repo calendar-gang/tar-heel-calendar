7b160213d2a52ccce46ba2719380c8d30c148e3e
Ezri White <ezri@live.unc.edu>
2020-11-15 11:42:45 -0500

    Added control bar to week view

--- src/components/week.jsx
+++ src/components/week.jsx
@@ -55,8 +55,15 @@ class Week extends Component {
             <div className="calendar">
                 <div className="container">
                     <section className="level" style={{ backgroundColor: "#b5e3f8", height: "50px" }}>
-                        <h1 class="title level-left has-text-light" style={{ margin: "10px" }}>Week of 11/15/20 </h1>
-                        <a className="button is-light level-right">New Entry</a>
+                        <div className="level-left">
+                            <h1 class="has-text-light" style={{ margin: "10px" }}>prev</h1>
+                            <h1 class="title has-text-light" style={{ margin: "10px" }}>Week of 11/15/20 </h1>
+                        </div>
+                        <div className="level-right">
+                            <a className="button is-light">New Entry</a>
+                            <h1 class="has-text-light" style={{ margin: "10px" }}>next</h1>
+                        </div>
+
                     </section>
                 </div>
                 <div className="container">
