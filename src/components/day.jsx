6fa7ce1091fa436320327b7c245badf6dcf7becc
Tori Hoffmann <torih@live.unc.edu>
2020-11-16 22:51:28 -0500

    added login/signup forms + todo list

--- src/components/day.jsx
+++ src/components/day.jsx
@@ -30,36 +30,88 @@ class Day extends Component {
         )
     }
 
+    toggletaskform(event) {
+        event.persist();
+        let name = event.target.id;
+        let edit_box = document.getElementById("newtask");
+
+        if (edit_box.className == "modal is-active") {
+            edit_box.className = "modal";
+        } else {
+            edit_box.className = "modal is-active";
+        }
+    }
+
+    rendertaskform() {
+        return (<div id="newtask" class="modal">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+            <header class="modal-card-head" >
+                <p class="modal-card-title">Create a new task:</p>
+                <button onClick={this.toggletaskform} class="delete" aria-label="close"></button>
+            </header>
+            <div class="control">
+                <input class="input" type="text" placeholder="New Task"/>
+            </div>
+            <section id="login-box-content" class="modal-card-body">
+            </section>
+            <footer class="modal-card-foot">
+                <button class="button create" id="createtask" onClick={this.toggletaskform}>Create</button>
+            </footer>
+            
+        </div>
+    </div>)
+    }
+
     render() {
         return (
-            <div className="calendar">
-                <div className="container">
-                    <section className="level" style={{ backgroundColor: "#b5e3f8", height: "50px" }}>
-                        <div className="level-left">
-                            <h1 class="has-text-light" style={{ margin: "10px" }}>prev</h1>
-                            <h1 class="title has-text-light" style={{ margin: "10px" }}>Sunday 11/15/20 </h1>
-                        </div>
-                        <div className="level-right">
-                            <a className="button is-light">New Entry</a>
-                            <h1 class="has-text-light" style={{ margin: "10px" }}>next</h1>
-                        </div>
+            <div className="daysview columns" style={{margin: "0px"}}>
+                <div className="calendar column">
+                    <div className="container">
+                        <section className="level" style={{ backgroundColor: "#b5e3f8", height: "50px" }}>
+                            <div className="level-left">
+                                <h1 class="has-text-light" style={{ margin: "20px" }}>prev</h1>
+                                <h1 class="title has-text-light" style={{ margin: "20px" }}>Sunday 11/15/20 </h1>
+                            </div>
+                            <div className="level-right">
+                                <a className="button is-light">New Entry</a>
+                                <h1 class="has-text-light" style={{ margin: "10px" }}>next</h1>
+                            </div>
 
-                    </section>
-                </div>
-                <div className="container">
-                    <table className="table is-bordered is-narrow is-hoverable is-fullwidth">
-                        <thead>
-                            <tr className="is-bordered">
-                                <th></th>
-                                <th className="has-text-grey-light" >Tasks</th>
-                            </tr>
-                        </thead>
-                        {this._renderBody()}
-                    </table>
-                </div>
+                        </section>
+                    </div>
+                    <div className="container" style={{ margin: "10px" }}>
+                        <table className="table is-bordered is-narrow is-hoverable is-fullwidth">
+                            <thead>
+                                <tr className="is-bordered">
+                                    <th></th>
+                                    <th className="has-text-grey-light" >Tasks</th>
+                                </tr>
+                            </thead>
+                            {this._renderBody()}
+                        </table>
+                    </div>
 
+                </div>
+                <div className="dailytodo container column box is-one-quarter" style={{ backgroundColor: "#0b1846"}}>
+                        <section className="level" style={{ backgroundColor: "#0b1846", height: "50px" }}>
+                            <h1 class="title has-text-light" style={{ margin: "10px" }}>My Daily To-Do:</h1>
+                        </section>
+                    <div className="container tasklist box" style={{ backgroundColor: "white", height: "95%", margin: "15px"}}>
+                        <a className="button taskmaker" id="newtaskbutton" style={{ margin: "10px",  backgroundColor: "gray", color: "white"}} onClick={this.toggletaskform}><strong>Add a task!</strong></a><br/>
+                        <div style={{margin: "10px"}}>
+                            <input type="checkbox" id="task1" name="task1"/>
+                            <label for="task1" class="task"> Be able to make new tasks</label><br/>
+                        </div>
+                        <div style={{margin: "10px"}}>
+                            <input type="checkbox" id="task2" name="task2"/>
+                            <label for="task2" class="task"> start adding data!!</label><br/>
+                        </div>
+                        {this.rendertaskform()}
+                    </div>
+                </div>
             </div>
-        )
+            )
     }
 }
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,6 +4,103 @@ import '../App.css'
 class NavBar extends Component {
     state = {};
 
+    toggleSUBox(event) {
+        event.persist();
+        let edit_box = document.getElementById("signup-box");
+        if (edit_box.className == "modal is-active") {
+            edit_box.className = "modal";
+        } else {
+            edit_box.className = "modal is-active";
+        }
+    }
+
+    renderSignUp() {
+        const header_style = {
+            backgroundColor: "#b5e3f8",
+            height: "75px"
+
+
+        }
+        const center = {
+            left: "265px"
+        }
+        return (
+            <div id="signup-box" class="modal">
+                <div class="modal-background"></div>
+                <div class="modal-card">
+                    <header class="modal-card-head" style={header_style}>
+                        <p class="modal-card-title">Welcome new user! Create an account below:</p>
+                        <button onClick={this.toggleSUBox} class="delete" aria-label="close"></button>
+                    </header>
+                    <div class="form modal-card-body" style={{height: "200px"}}>
+                        <label class="label">New Username</label>
+                        <div class="control">
+                            <input class="input" type="text"></input>
+                        </div>
+                        <label class="label">New Password</label>
+                        <div class="control">
+                            <input class="input" type="text"></input>
+                        </div>
+                    </div>
+                    <footer class="modal-card-foot" style={header_style}>
+                        <button class="button login" onClick={this.toggleSUBox} style={center}>Sign Up</button>
+                    </footer>
+                </div>
+            </div>
+
+
+        )
+    }
+
+    toggleLoginBox(event) {
+        event.persist();
+        let edit_box = document.getElementById("login-box");
+        if (edit_box.className == "modal is-active") {
+            edit_box.className = "modal";
+        } else {
+            edit_box.className = "modal is-active";
+        }
+    }
+
+    renderLogin() {
+        const header_style = {
+            backgroundColor: "#b5e3f8",
+            height: "75px"
+
+        }
+        const center = {
+            left: "275px"
+        }
+
+        return (
+            <div id="login-box" class="modal">
+                <div class="modal-background"></div>
+                <div class="modal-card">
+                    <header class="modal-card-head" style={header_style}>
+                        <p class="modal-card-title">Let's Get Productive!</p>
+                        <button onClick={this.toggleLoginBox} class="delete" aria-label="close"></button>
+                    </header>
+                    <div class="form modal-card-body" style={{height:"200px"}}>
+                        <label class="label">Username</label>
+                        <div class="control">
+                            <input class="input" type="text"></input>
+                        </div>
+                        <label class="label">Password</label>
+                        <div class="control">
+                            <input class="input" type="text"></input>
+                        </div>
+                    </div>
+                    <footer class="modal-card-foot" style={header_style}>
+                        <button class="button login" onClick={this.toggleLoginBox} style={center}>Log in</button>
+                    </footer>
+                    
+                </div>
+            </div>
+
+
+        )
+    }
+
     render() {
         return (
             <nav id="top-nav" className="navbar" aria-label="main-navigation">
@@ -12,17 +109,24 @@ class NavBar extends Component {
                         <h1 className="title is-1" >TAR HEEL CALENDAR</h1>
                     </a>
                 </div>
-                <div className="navbar-end">
+                <div className="navbar-end" id="navbarend">
                     <div className="navbar-item">
                         <div className="buttons">
-                            <a className="button" style={{ backgroundColor: "#b5e3f8" }}>
-                                Sign Up
-                            </a>
-                            <a className="button is-light">
-                                Log in
-                            </a>
-                        </div>
+                            <div className= "signup">
+                                <a className="button" id="signupbutton" style={{ backgroundColor: "#b5e3f8" }} onClick={this.toggleSUBox}>
+                                    Sign Up
+                                </a>
+                                { this.renderSignUp()}
+                            </div>
+                            <div className= "login">
+                                <a className="button is-light" id="loginbutton" onClick={this.toggleLoginBox}>
+                                    <p id="logintext">Log in</p>
 
+                                </a>
+                            </div>
+                            { this.renderLogin()}
+                        </div>
+                        
                     </div>
                 </div>
 
