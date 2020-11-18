68f7126931ae657fa64f307140b7c7a6b7102ec8
Eric Schneider <u1643364@live.unc.edu>
2020-11-18 00:40:19 -0500

    Added logout function

---
+++ docs/logout.md
@@ -0,0 +1,51 @@
+# logout
+This file describes the `/logout` endpoint.
+
+## Core Details
+* Endpoint: `/logout`.
+* Request method: `POST`.
+* Body parameters:
+    * `token`: string of 60 characters
+
+## Example operations
+### Example (valid input)
+```js
+let res = await axios.post('tar-heel-calendar.herokuapp.com/register', {
+    token: 'b15efc04cb19d956fc982451343981ecf956dc31eab7486a4d7939a77476'
+});
+```
+
+#### Response (status: 200)
+```json
+{
+    "message": "Deleted token."
+}
+```
+
+### Example (no token)
+```js
+let res = await axios.post('tar-heel-calendar.herokuapp.com/register', {
+    token: 'ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ'
+});
+```
+
+#### Response (status: 400)
+```json
+{
+    "message": "Token not found."
+}
+```
+
+### Example (bad length)
+```js
+let res = await axios.post('tar-heel-calendar.herokuapp.com/register', {
+    token: 'b15efc04cb19d956fc982451343'
+});
+```
+
+#### Response (status: 400)
+```json
+{
+    "message": "Invalid length of parameter."
+}
+```
--- src/server.js
+++ src/server.js
@@ -139,6 +139,44 @@ app.post('/login', (req, res) => {
     });
 });
 
+app.post('/logout', (req, res) => {
+    const { token } = req.body;
+
+    if(!isStringValidLength(token, 60, 60)){
+        res.status(400);
+        res.json({
+            message: "Invalid length of parameter."
+        });
+
+        return;
+    }
+
+    db.query(`SELECT token
+            FROM tokens
+            WHERE token=?`, [token], (error, results, fields) => {
+        if(error) throw error;
+
+        if(results.length=== 0){
+            res.status(400);
+            res.json({
+                message: "Token not found."
+            });
+
+            return;
+        }
+
+        db.query(`DELETE
+                FROM tokens
+                WHERE token=?`, [token], (error, results, fields) => {
+            if(error) throw error;
+
+            res.json({
+                message: "Deleted token."
+            });
+        });
+    });
+});
+
 function isStringValidLength(str, min, max){
     return str !== undefined && typeof(str) === "string" && str.length >= min && str.length <= max;
 }
