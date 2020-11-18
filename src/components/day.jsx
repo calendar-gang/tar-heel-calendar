414604795ac87ab85d49f9c50b14789102dad1d4
Tori Hoffmann <torih@live.unc.edu>
2020-11-17 23:39:51 -0500

    Todo list and date updates

--- src/components/day.jsx
+++ src/components/day.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { useState } from 'react';
 import Task from './task';
 import ReactDOM from 'react-dom'
 
 class Day extends Component {
     state = {}
+    tasklist = ["get", "this", "to", "work"]
 
     _findHour(time) {
         if (time < 12) { return time === 0 ? "12 AM" : time + " AM" }
@@ -46,52 +48,77 @@ class Day extends Component {
 
     createTask(event) {
         let tasktext = this.refs.tasktext.value;
+        this.tasklist.push(tasktext)
         // window.alert(tasktext)
         this.toggletaskform(event)
         const d = document.createElement("div")
         const id = Math.random() 
         d.id = id
         document.getElementById('newtasks').appendChild(d)
-        ReactDOM.render((<div class="box" style={{margin: "10px"}}>
-        <input type="checkbox"/>
-        <label class="task" style={{marginLeft: "5px"}}>{tasktext}</label><br/>
-        </div>), document.getElementById(id));
+        // ReactDOM.render((<div className="box" style={{margin: "10px"}}>
+        // <input type="checkbox"/>
+        // <label className="task" style={{marginLeft: "5px"}}>{tasktext}</label><br/>
+        // </div>), document.getElementById(id));
+        ReactDOM.render(<Task text={tasktext}></Task>, document.getElementById(id));
     }
 
+    // strikeTask(event) {
+    //     ReactDOM.render((<div><label for="task2" className="task" id="task2text" style={{textDecoration: 'line-through'}}> start adding data!!</label>
+    //                             <p>Task Complete!</p></div>), document.getElementById("task2text"));
+    // }
+
     rendertaskform() {
-        return (<div id="newtask" class="modal">
-        <div class="modal-background"></div>
-        <div class="modal-card">
-            <header class="modal-card-head" >
-                <p class="modal-card-title">Create a new task:</p>
-                <button onClick={this.toggletaskform} class="delete" aria-label="close"></button>
+        return (<div id="newtask" className="modal">
+        <div className="modal-background"></div>
+        <div className="modal-card">
+            <header className="modal-card-head" >
+                <p className="modal-card-title">Create a new task:</p>
+                <button onClick={this.toggletaskform} className="delete" aria-label="close"></button>
             </header>
-            <div class="control">
-                <input class="input" ref="tasktext" type="text" placeholder="New Task"/>
+            <div className="control">
+                <input className="input" ref="tasktext" type="text" placeholder="New Task"/>
             </div>
-            <section id="login-box-content" class="modal-card-body">
+            <section id="login-box-content" className="modal-card-body">
             </section>
-            <footer class="modal-card-foot">
-                <button class="button create" id="createtask" onClick={this.createTask.bind(this)}>Create</button>
+            <footer className="modal-card-foot">
+                <button className="button create" id="createtask" onClick={this.createTask.bind(this)}>Create</button>
             </footer>
             
         </div>
     </div>)
     }
 
+    rendercurrenttasks() {
+        const tasks = []
+        for (let i = 0; i < this.tasklist.length; i++) {
+            tasks.push(<Task text={this.tasklist[i]}></Task>)
+        }
+        return tasks
+    }
+
     render() {
+
+        var now = new Date();
+        var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+        var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+        var day = days[ now.getDay() ];
+        var month = months[ now.getMonth() ];
+
+        let writtendate = day + ", " + month + " " + now.getDate() + "th"
+
         return (
             <div className="daysview columns" style={{margin: "0px"}}>
                 <div className="calendar column">
                     <div className="container">
                         <section className="level" style={{ backgroundColor: "#b5e3f8", height: "50px" }}>
                             <div className="level-left">
-                                <h1 class="has-text-light" style={{ margin: "20px" }}>prev</h1>
-                                <h1 class="title has-text-light" style={{ margin: "20px" }}>Sunday 11/15/20 </h1>
+                                <h1 className="has-text-light" style={{ margin: "20px" }}>prev</h1>
+                                <h1 className="title has-text-light" style={{ margin: "20px" }}>{writtendate} </h1>
                             </div>
                             <div className="level-right">
                                 <a className="button is-light">New Entry</a>
-                                <h1 class="has-text-light" style={{ margin: "10px" }}>next</h1>
+                                <h1 className="has-text-light" style={{ margin: "10px" }}>next</h1>
                             </div>
 
                         </section>
@@ -111,18 +138,10 @@ class Day extends Component {
                 </div>
                 <div className="dailytodo container column box is-one-quarter" style={{ backgroundColor: "#0b1846"}}>
                         <section className="level" style={{ backgroundColor: "#0b1846", height: "50px" }}>
-                            <h1 class="title has-text-light" style={{ margin: "10px" }}>My Daily To-Do:</h1>
+                            <h1 className="title has-text-light" style={{ margin: "10px" }}>My Daily To-Do:</h1>
                         </section>
                     <div className="container tasklist box" id="tasklist" style={{ backgroundColor: "white", height: "95%", margin: "15px"}}>
-                        <a className="button taskmaker" id="newtaskbutton" style={{ margin: "10px",  backgroundColor: "gray", color: "white"}} onClick={this.toggletaskform}><strong>Add a task!</strong></a><br/>
-                        <div class="box" style={{margin: "10px"}}>
-                            <input type="checkbox" id="task1" name="task1"/>
-                            <label for="task1" class="task"> Be able to make new tasks</label><br/>
-                        </div>
-                        <div class="box" style={{margin: "10px"}}>
-                            <input type="checkbox" id="task2" name="task2"/>
-                            <label for="task2" class="task"> start adding data!!</label><br/>
-                        </div>
+                        {this.rendercurrenttasks()}
                         <div id="newtasks"></div>
                         {this.rendertaskform()}
                     </div>
@@ -134,3 +153,4 @@ class Day extends Component {
 
 export default Day;
 
+// <input type="checkbox" id="task2" name="task2" onClick={this.strikeTask.bind(this)}/>
