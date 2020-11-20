cab937411f8c6ac973c74b9ff86554210f548099
Eric Schneider <u1643364@live.unc.edu>
2020-11-19 21:40:21 -0500

    Events table seems good enough for now. Going to save befor trying to modularize

--- scripts/create/sql/create_events.sql
+++ scripts/create/sql/create_events.sql
@@ -5,6 +5,9 @@ CREATE TABLE IF NOT EXISTS `events` (
     `description` TEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci',
     `start` TIMESTAMP NOT NULL,
     `end` TIMESTAMP NOT NULL,
+    `recurring` ENUM('not','weekly','monthly','yearly') NOT NULL DEFAULT 'not' COLLATE 'utf8_general_ci',
+    `recurringuntil` TIMESTAMP NULL DEFAULT NULL,
+    `category` ENUM('default','school') NOT NULL DEFAULT 'default' COLLATE 'utf8_general_ci',
     CONSTRAINT `username_events`
         FOREIGN KEY (`username`) REFERENCES `users` (`username`)
 )
