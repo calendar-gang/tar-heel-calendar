01e46d3a9c7132965d18445beeaf036c33e97d5a
Tori Hoffmann <torih@live.unc.edu>
2020-11-21 20:54:58 -0500

    trying again

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -95,7 +95,7 @@ class NavBar extends Component {
         console.log(result["message"])
         console.log(result.message)
         console.log(results.data)
-        if (result["message"] === "Registration complete.") {
+        if (result.message === "Registration complete.") {
             this.setState({messagefield: "Successfully registered!"})
             this.toggleMessage()
 
@@ -117,7 +117,7 @@ class NavBar extends Component {
         console.log(result["message"])
         console.log(result.message)
         console.log(results.data)
-       if (result["message"] === "Logged in.") {
+       if (result.message === "Logged in.") {
            this.setState({messagefield: "Successfully logged in!"})
            this.toggleMessage()
        }
