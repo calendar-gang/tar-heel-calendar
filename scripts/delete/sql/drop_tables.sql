36d74ac731071c34abe65ea67ba24b643a211d3c
Eric Schneider <u1643364@live.unc.edu>
2020-11-21 23:06:05 -0500

    Created db components of tasks

--- scripts/delete/sql/drop_tables.sql
+++ scripts/delete/sql/drop_tables.sql
@@ -1,1 +1,2 @@
-DROP TABLE IF EXISTS `events`, `tokens`, `users`;
+/* Note that the order is in fact important here, as earlier tables rely on previous tables. */
+DROP TABLE IF EXISTS `tasks`, `events`, `tokens`, `users`;
