830b4707c0c083b02478d396086e6af24389754e
Tori Hoffmann <torih@live.unc.edu>
2020-11-24 00:52:20 -0500

    day colors

--- src/components/day.jsx
+++ src/components/day.jsx
@@ -121,7 +121,7 @@ class Day extends Component {
                     name: events[i].title,
                     location: events[i].location,
                     description: events[i].description,
-                    category: i % 9
+                    category: events[i].category
                 })
             }
             this.setState({ eventlist: elist });
