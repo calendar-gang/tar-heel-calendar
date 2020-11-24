26e7112a01fd55649bc116bc53e5cb764959ec38
Alfred Mathew <alfredmathew@outlook.com>
2020-11-24 00:25:39 -0500

    Revert "update state using setState"
    This reverts commit 7f8dbd88041b6f8d10772fcf3fc76927c7544ae1.

--- src/components/day.jsx
+++ src/components/day.jsx
@@ -129,6 +129,7 @@ class Day extends Component {
             }
             this.setState({ eventlist: elist });
         }
+        return elist;
     }
 
 
@@ -136,11 +137,7 @@ class Day extends Component {
         for (let i = 0; i < this.state.eventlist.length; i++) {
             let evt = this.state.eventlist[i];
             let day_evt = <DayEvent eventstate={evt}></DayEvent>;
-            this.setState((state) => {
-                let day_events = Object.assign({}, state.dayEvents);
-                day_events[`${evt.start}`].push(day_evt);
-                return {dayEvents: day_events}
-            });
+            this.state.dayEvents[`${evt.start}`].push(day_evt);
         }
     }
 
@@ -178,17 +175,11 @@ class Day extends Component {
         let current_events = [...this.state.eventlist];
         current_events.push(obj);
         let day_evt = <DayEvent eventstate={obj}></DayEvent>;
-        this.setState((state) => {
-            let day_events = Object.assign({}, state.dayEvents);
-            day_events[`${evt.start}`].push(day_evt);
-            return {
-                dayEvents: day_events,
-                eventlist: current_events
-            }
+        this.state.dayEvents[`${obj.start}`].push(day_evt);
+        this.setState({
+            eventlist: current_events,
+            dayEvents: this.state.dayEvents
         });
-        // this.setState({
-        //     eventlist: current_events
-        // });
     }
 
 
@@ -354,9 +345,7 @@ class Day extends Component {
 
         await this._getcurrentevents(new_date_object);
         await this._getcurrenttasks();
-        this.setState({ 
-            date: new_date_object,
-        });
+        this.setState({ date: new_date_object });
 
 
         // this.state.cache[this.state.date] = {
